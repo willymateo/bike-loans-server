2f31fe08ae0d573ba49f89f724f76c6b074a81f0
willymateo <wjmateo@espol.edu.ec>
2023-08-19 14:34:35 -0500

    Implement flask server

---
+++ app.py
@@ -0,0 +1,34 @@
+#  from sklearn.preprocessing import MinMaxScaler
+from flask import Flask, request, jsonify
+
+#  from keras.models import load_model
+#  import numpy as np
+
+app = Flask(__name__)
+#  model = load_model("loans_model.h5")
+
+
+@app.get("/")
+def home():
+    return {"message": "Bike loans"}
+
+
+@app.post("/predict")
+def predict_loans():
+    try:
+        data = request.get_json(force=True)
+        preprocessed_data = preprocess_input(data)
+
+        prediction = model.predict(preprocessed_data)
+        loans = prediction[0]
+
+        return jsonify(loans)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
+def preprocess_data():
+    return preprocessed_data
+
+
+app.run(debug=True)
